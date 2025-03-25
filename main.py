3ed2dfb4699904c6b8eaa4ffad9c388054c32815
antonbloxin <antonblokhin@mail.ru>
2025-03-25 14:43:18 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,39 +1,47 @@
-import asyncio
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+import logging
+import os
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-async def start(update: Update, context) -> None:
-    # Отправляем приветственное сообщение
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
+# Define a few command handlers. These usually take the two arguments update and
+# context.
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /start is issued."""
     await update.message.reply_text("🏛️ Я Гермес! Бот проекта 300 Терм. Помогу вам получить нужные материалы.")
 
-    # Задержка 3 секунды перед отправкой меню
-    await asyncio.sleep(3)
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a message when the command /help is issued."""
+    await update.message.reply_text("Help!")
 
-    # Определяем кнопки меню
-    keyboard = [
-        [InlineKeyboardButton("📄 Получить КП", callback_data='get_kp')],
-        [InlineKeyboardButton("📑 Получить Техусловия", callback_data='get_tech')],
-        [InlineKeyboardButton("📊 Получить Презентацию", callback_data='get_presentation')],
-        [InlineKeyboardButton("🎥 Посмотреть Видео", callback_data='watch_video')],
-        [InlineKeyboardButton("📂 Посмотреть кейсы", callback_data='view_cases')],
-        [InlineKeyboardButton("📞 Контакты", callback_data='contacts')]
-    ]
-
-    # Создаем разметку для меню
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    # Отправляем меню пользователю
-    await update.message.reply_text("Выберите опцию:", reply_markup=reply_markup)
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    await update.message.reply_text(update.message.text)
 
 def main() -> None:
     """Start the bot."""
-    application = Application.builder().token("YOUR_BOT_TOKEN").build()
+    # Create the Application and pass it your bot's token.
+    application = Application.builder().token(
+        os.environ.get("TOKEN")
+    ).build()
 
-    # on /start command - trigger the start function
+    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+
+    # on non command i.e message - echo the message on Telegram
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
     # Run the bot until the user presses Ctrl-C
-    application.run_polling()
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
